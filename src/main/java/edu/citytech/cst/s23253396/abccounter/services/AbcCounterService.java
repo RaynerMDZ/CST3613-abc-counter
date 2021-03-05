d2d599b67ccb80c08869c824bb06d3cebd9bbf94
Rayner Mendez <raynermendezg@gmail.com>
2021-03-05 12:36:42 -0500

    Add documentation to AbcCounterService class.

--- src/main/java/edu/citytech/cst/s23253396/abccounter/services/AbcCounterService.java
+++ src/main/java/edu/citytech/cst/s23253396/abccounter/services/AbcCounterService.java
@@ -9,6 +9,10 @@ public class AbcCounterService {
 
     private final static Pattern alphabet = Pattern.compile("^[a-zA-Z]*$");
 
+    /**
+     * This method returns a list of characters of the alphabet. From A to Z both uppercase and lowercase.
+     * @return List<Characters>
+     */
     public List<Character> countABC() {
         List<Character> abc = new ArrayList<>();
 
@@ -20,6 +24,10 @@ public class AbcCounterService {
         return abc;
     }
 
+    /**
+     * This method returns a list of integers from n to m.
+     * @return List<Integers>
+     */
     public  List<Integer> count123() {
         List<Integer> integers = new ArrayList<>();
         for (int i = 1; i <= 50; i++) {
@@ -28,6 +36,12 @@ public class AbcCounterService {
         return integers;
     }
 
+    /**
+     *  You can use this same function to check for consonants. Just add ! in the controller to check whether a
+     *  letter is a vowel or not.
+     * @param string which is a letter from the gui.
+     * @return boolean
+     */
     public boolean isVowel(String string) {
         // Java 8 Functional Programming
         return Stream.of("a", "e", "i", "o", "u")
